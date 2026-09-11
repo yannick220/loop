41a0b2aba7e5afc5eec2918e0a36d5dd2232190e
yannick220 <103240663+yannick220@users.noreply.github.com>
2026-09-11 18:30:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='loop-v3-photos-nav-20260911';
+const CACHE='loop-v5-hierarchy-history-20260911';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
-'./assets/avatar_0.png','./assets/avatar_1.png','./assets/avatar_2.png','./assets/avatar_3.png','./assets/avatar_4.png','./assets/avatar_5.png',
+'./assets/photo_0.jpg','./assets/photo_1.jpg','./assets/photo_2.jpg','./assets/photo_3.jpg','./assets/photo_4.jpg','./assets/photo_5.jpg',
 './assets/loop_0.png','./assets/loop_1.png','./assets/loop_2.png','./assets/loop_3.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
