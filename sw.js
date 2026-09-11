558f2fb51decdc3a2766f3c29e3e2a92e1aee2cc
yannick220 <103240663+yannick220@users.noreply.github.com>
2026-09-11 16:00:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='loop-v2-neon-20260911';
+const CACHE='loop-v3-photos-nav-20260911';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
 './assets/avatar_0.png','./assets/avatar_1.png','./assets/avatar_2.png','./assets/avatar_3.png','./assets/avatar_4.png','./assets/avatar_5.png',
 './assets/loop_0.png','./assets/loop_1.png','./assets/loop_2.png','./assets/loop_3.png'];
